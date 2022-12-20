e13d82840a8a291d10fe16fedc9529f8bedb00cc
olivier kermoal <olivier.kermoal@gmail.com>
2022-12-20 12:38:45 +0100

    Seed done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
+
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'This Movie is already in the list' }
+  validates :comment, length: { minimim: 6 }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  has_many :boomarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+  validates :name, uniqueness: true
 end
